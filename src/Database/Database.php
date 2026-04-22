3da6ae2751953ba6eb87d6dcd9d4ecf599f45247
lamaliabdallah <lamaliabdalah90@gmail.com>
2026-04-22 10:26:21 +0200

    cinetech

---
+++ src/Database/Database.php
@@ -0,0 +1,30 @@
+<?php
+namespace CINETECH\Database;
+class Database {
+    private static $instance;
+    private \PDO $pdo;
+
+    private function __construct() {
+        $this->pdo = new \PDO(
+            "mysql:host=lamali-abdallah.students-laplateforme.io;dbname=lamali-abdallah_lamali;charset=utf8",
+            'lamali',
+            'lamali26100',
+            [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]
+        );
+    }
+
+    public static function getInstance() {
+        if (self::$instance === NULL) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+    public function getConnexion(): \PDO{
+        return $this->pdo;
+    }
+}
+
+
+
+
+?>
